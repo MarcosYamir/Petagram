8651a1c32b421ecf631eebbbff93b3b49085a423
MarcosYamir <marcos.yamir@gmail.com>
2016-05-13 15:33:29 -0500

    Initial commit

---
+++ app/src/main/java/mx/marcosyamir/firstpetagram/DetalleMascota.java
@@ -0,0 +1,17 @@
+package mx.marcosyamir.firstpetagram;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by UPFIM-SIMyVC2 on 13/05/2016.
+ */
+public class DetalleMascota extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.mascota_detalle);
+    }
+}
